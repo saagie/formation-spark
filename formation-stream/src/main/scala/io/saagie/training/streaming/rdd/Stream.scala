ac9a34d01aad5997b634adb1f08b1b531e5f8a64
Erwan KOFFI <erwan@saagie.com>
2018-06-04 23:51:48 +0200

    * Comments and structure

--- formation-stream/src/main/scala/io/saagie/training/streaming/rdd/Stream.scala
+++ formation-stream/src/main/scala/io/saagie/training/streaming/rdd/Stream.scala
@@ -1,3 +1,5 @@
+package io.saagie.training.streaming.rdd
+
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
@@ -7,14 +9,20 @@ import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
 import org.json4s.DefaultFormats
 import org.json4s.jackson.Serialization.read
 
+//Request class matching data in Kafka
 case class Request(origin: String, ip: String, userAgent: Option[String], timestamp: Long, latitude: Double, longitude: Double, price: Double)
 
 object Stream extends App {
+  //Sets the Hadoop user name
   System.setProperty("HADOOP_USER_NAME", "hdfs")
 
+  //Initializing the spark configuration
   val sparkConf = new SparkConf()
     .setAppName("Spark Formation")
+    .set("spark.ui.showConsoleProgress", "false")
+    .set("spark.executor.extraJavaOptions", "-Dlog4j.configuration=log4j.xml")
 
+  //Kafka configuration
   val kafkaParams = Map[String, Object](
     "bootstrap.servers" -> "192.168.61.50:31200",
     "key.deserializer" -> classOf[StringDeserializer],
@@ -24,19 +32,22 @@ object Stream extends App {
     "enable.auto.commit" -> (false: java.lang.Boolean)
   )
 
+  //Context initialisation
   val streamingContext = new StreamingContext(sparkConf, Seconds(1))
-  streamingContext.sparkContext.setLogLevel("ERROR")
 
+  //Creation of the stream
   val stream = KafkaUtils.createDirectStream[String, String](
     streamingContext,
     PreferConsistent,
     Subscribe[String, String](Array("spark"), kafkaParams)
   )
 
+  //Regex initialization
   val opera = ".*Opera.*".r
   val chrome = ".*Chrome.*".r
   val firefox = ".*Firefox.*".r
 
+  //Function that will transform user agent into a simple navigator string
   def nav(ua: String): String = {
     ua match {
       case opera() => "Opera"
@@ -46,28 +57,43 @@ object Stream extends App {
     }
   }
 
+  //Format used by json4s
   implicit val formats = DefaultFormats
-  stream
-    .transform(_.map(_.value()))
-    .transform(rdd => if (!rdd.isEmpty()) {
-      rdd.map(read[Request])
-    } else {
-      streamingContext.sparkContext.emptyRDD[Request]
-    })
-    .window(Minutes(5))
-    .map(r => (nav(r.userAgent.getOrElse("Aucun")), r.price))
-    .foreachRDD { rdd => {
-      val to = rdd
-        .groupBy(_._1)
-        .reduceByKey(_ ++ _)
-        .mapValues(v => (v.size, v.aggregate(0.0)((a, b) => a + b._2, _ + _) / v.size))
-        .map(tuple => (tuple._1, tuple._2._1, tuple._2._2))
-        .sortBy(-_._2)
-      println(to.collect().toSeq)
-      to.saveAsTextFile(s"hdfs://cluster/user/hdfs/spark/requests-${System.currentTimeMillis()}")
-    }
-    }
 
+  //Initialization of the stream
+  stream
+    //We transform the stream to get only the value
+    .transform(_.map(_.value()))
+    //If the rdd is not empty, we parse the JSON
+    .transform(rdd => if (!rdd.isEmpty()) {
+    rdd.map(read[Request])
+  } else {
+    streamingContext.sparkContext.emptyRDD[Request]
+  })
+    //Creation of a 5 minutes window
+    .window(Minutes(5))
+    //Map the navigator name with a price
+    .map(r => (nav(r.userAgent.getOrElse("Aucun")), r.price))
+    //For each rdd write the file
+    .foreachRDD { rdd => {
+    val to = rdd
+      .groupBy(_._1) //Group by navigators
+      .reduceByKey(_ ++ _) //Sum all the prices
+      .mapValues(v => (v.size, v.aggregate(0.0)((a, b) => a + b._2, _ + _) / v.size)) //Aggregation to get the mean
+      .map(tuple => (tuple._1, tuple._2._1, tuple._2._2)) //Change tuple format from (String, (Int, Double)) to (String, Int, Double)
+      .sortBy(-_._2) //Sort by price
+
+    //Print of each rdd /!\ Really bad on big volume of data
+    println(to.collect().toSeq)
+
+    //Write data into a file
+    to.saveAsTextFile(s"/user/formation-spark/streaming/requests-${System.currentTimeMillis()}")
+  }
+  }
+
+  //Start of streaming context
   streamingContext.start()
+
+  //Waiting the termination of the stream (Hangs for exception or execution stop)
   streamingContext.awaitTermination()
 }
--- formation-stream/src/main/scala/io/saagie/training/streaming/sql/StreamSQL.scala
+++ formation-stream/src/main/scala/io/saagie/training/streaming/sql/StreamSQL.scala
@@ -1,3 +1,6 @@
+package io.saagie.training.streaming.sql
+
+import io.saagie.training.streaming.rdd.Request
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.OutputMode
 import org.apache.spark.sql.types.{StringType, TimestampType}
@@ -5,31 +8,37 @@ import org.json4s.DefaultFormats
 import org.json4s.jackson.Serialization.read
 
 object StreamSQL extends App {
+  //Initialize a Spark Session
   val spark = SparkSession.builder()
     .appName("SQL Kafka")
+    .config("spark.ui.showConsoleProgress", "false")
+    .config("spark.executor.extraJavaOptions", "-Dlog4j.configuration=log4j.xml")
     .getOrCreate()
-  spark.sparkContext.setLogLevel("ERROR")
 
+  //Import sql functions
+  import org.apache.spark.sql.functions._
   import spark.implicits._
 
+  //Open a stream to a kafka broker
   val df = spark
     .readStream
     .format("kafka")
+    .option("startingOffsets", "earliest")
     .option("kafka.bootstrap.servers", "192.168.61.50:31200")
     .option("subscribe", "spark")
     .load()
 
+  //json4s formats
   implicit val formats = DefaultFormats
 
-  import org.apache.spark.sql.functions._
-
+  //Regexp used by udf
   val opera = ".*Opera.*".r
-  val safari = ".*Safari.*".r
   val chrome = ".*Chrome.*".r
   val firefox = ".*Firefox.*".r
 
+  //We create an udf to transform each user agent into a navigator String
   val toNav = udf((ua: String) => {
-    if (ua != null) {
+    if (ua != None.orNull) {
       ua match {
         case opera() => "Opera"
         case chrome() => "Chrome"
@@ -41,21 +50,31 @@ object StreamSQL extends App {
     }
   })
 
-  val values = df
+  //Select the value of the dataframe
+  val stringValue = df
     .select($"value" cast StringType)
+
+  //Map into a request object and therefore a Dataset
+  val ds = stringValue
     .map(a => read[Request](a.getString(0)))
+
+  /* Apply the udf to our request, group by browsers over a window of 10 seconds,
+     based on request's timestamp's and compute average */
+  val prices = ds
     .select(toNav($"userAgent") as "browser", $"timestamp" cast TimestampType, $"price")
-    .groupBy($"browser", window($"timestamp", "5 minutes"))
+    .groupBy($"browser", window($"timestamp", "10 seconds"))
     .agg(count($"price"), avg($"price"))
 
-  values
-    .writeStream
-    .format("parquet")
-    .option("path", "hdfs://cluster/user/hdfs/spark/aggregation.parquet")
-    .outputMode(OutputMode.Complete())
-    .start()
+  //Write into a file not working with all Spark versions
+  //  prices
+  //    .writeStream
+  //    .format("parquet")
+  //    .option("path", "/user/formation-spark/streaming/sql")
+  //    .outputMode(OutputMode.Append())
+  //    .start()
 
-  values
+  //Write our aggregation into the console on each new value
+  prices
     .writeStream
     .format("console")
     .outputMode(OutputMode.Complete())
